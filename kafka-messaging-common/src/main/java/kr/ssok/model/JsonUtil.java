a0861e3631d8d8c52f1ef54149aa6af1f18ebab7
dhku <dhku@naver.com>
2026-02-22 21:00:12 +0900

    fix: DB의존성 제거

--- kafka-messaging-common/src/main/java/kr/ssok/model/JsonUtil.java
+++ kafka-messaging-common/src/main/java/kr/ssok/model/JsonUtil.java
@@ -1,5 +1,8 @@
 package kr.ssok.model;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.NoArgsConstructor;
 
 /**
